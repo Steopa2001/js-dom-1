d54f21752ca46cd5e9d9169f427c3915590894e1
Steopa2001 <moraru495@gmail.com>
2025-07-09 15:44:56 +0200

    feat: complete bonus

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,20 @@
 const bottone = document.getElementById('bottone');
 const lampada = document.getElementById('lamp');
 
+  //stato iniziale: lampadina spenta 
+  let lampOn = false;
+
+
 bottone.addEventListener('click', () => {
-  lampada.src = './assets/img/yellow_lamp.png';
+
+  if (lampOn === false) {
+    lampada.src = './assets/img/yellow_lamp.png';
+    bottone.textContent = 'Spegni';
+    lampOn = true;
+  } else {
+    lampada.src = './assets/img/white_lamp.png';
+    bottone.textContent = 'Accendi';
+    lampOn = false;
+  }
+
 });
