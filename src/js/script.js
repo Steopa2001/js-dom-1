21d4d6fd75aca19b62574a36eb1eabda1e3151ab
Steopa2001 <moraru495@gmail.com>
2025-07-09 15:54:21 +0200

    feat: redone exercse bonus with string.includes() method

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,38 @@
+// const bottone = document.getElementById('bottone');
+// const lampada = document.getElementById('lamp');
+
+//   //stato iniziale: lampadina spenta
+//   let lampOn = false;
+
+
+// bottone.addEventListener('click', () => {
+
+//   if (lampOn === false) {
+//     lampada.src = './assets/img/yellow_lamp.png';
+//     bottone.textContent = 'Spegni';
+//     lampOn = true;
+//   } else {
+//     lampada.src = './assets/img/white_lamp.png';
+//     bottone.textContent = 'Accendi';
+//     lampOn = false;
+//   }
+
+// });
+
 const bottone = document.getElementById('bottone');
 const lampada = document.getElementById('lamp');
 
-  //stato iniziale: lampadina spenta 
-  let lampOn = false;
-
-
 bottone.addEventListener('click', () => {
-
-  if (lampOn === false) {
+  // Controllo cosa c'è scritto sul bottone
+  if (bottone.textContent.includes('Accendi')) {
+    // Se c'è scritto Accendi, significa che la lampada è spenta
     lampada.src = './assets/img/yellow_lamp.png';
     bottone.textContent = 'Spegni';
-    lampOn = true;
   } else {
+    // Se c'è scritto Spegni, significa che la lampada è accesa
     lampada.src = './assets/img/white_lamp.png';
     bottone.textContent = 'Accendi';
-    lampOn = false;
   }
-
 });
+
+
